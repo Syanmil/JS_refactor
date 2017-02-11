14cbd1545a37f1380725e259548abe5974418c4a
Syanmil <syanmil@yahoo.com>
2017-02-11 16:07:50 +0700

    MVC ing javascript done

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
-  roller.onClickListener()
+  controller.onClickListener()
 })
-let roller = {
+
+let controller = {
+  onClickListener: function(){
+    $('#roller button.add').on('click', view.addDice)
+    $('#roller button.roll').on('click', view.rollDice)
+  },
+  getData: function(){
+    return model.randomSix()
+  }
+}
+
+let view = {
   addDice : function(){
     $('.dice').append('<div class="die">0</div>')
   },
   rollDice: function() {
     $('.die').each(function(k, die) {
-      $(die).text(roller.randomSix())
+      $(die).text(controller.getData)
     })
-  },
+  }
+}
+
+let model = {
   randomSix: function(){
     return Math.floor((Math.random()*6)+1)
-  },
-  onClickListener: function(){
-    $('#roller button.add').on('click', roller.addDice)
-    $('#roller button.roll').on('click', roller.rollDice)
   }
 }
